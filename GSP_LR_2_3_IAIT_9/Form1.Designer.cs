08701ec1476e2c40db92c8fa615ae4a85a3c1989
golanovma <golanovma@63pokupki.ru>
2022-10-09 13:48:18 +0400

    working version

--- GSP_LR_2_3_IAIT_9/Form1.Designer.cs
+++ GSP_LR_2_3_IAIT_9/Form1.Designer.cs
@@ -29,21 +29,132 @@ namespace GSP_LR_2_3_IAIT_9
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.GroupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.GroupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 58);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(838, 412);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // GroupBox1
+            // 
+            this.GroupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.GroupBox1.Controls.Add(this.label2);
+            this.GroupBox1.Controls.Add(this.comboBox2);
+            this.GroupBox1.Controls.Add(this.button2);
+            this.GroupBox1.Controls.Add(this.label1);
+            this.GroupBox1.Controls.Add(this.comboBox1);
+            this.GroupBox1.Location = new System.Drawing.Point(12, 12);
+            this.GroupBox1.Name = "GroupBox1";
+            this.GroupBox1.Size = new System.Drawing.Size(838, 40);
+            this.GroupBox1.TabIndex = 1;
+            this.GroupBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(291, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Способ заливки";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Построчный",
+            "Ориентированный"});
+            this.comboBox2.Location = new System.Drawing.Point(410, 10);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(156, 24);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button2.Location = new System.Drawing.Point(688, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Очистить полотно";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Цвет рисования";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.comboBox1.DisplayMember = "1";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Черный",
+            "Красный",
+            "Зеленый",
+            "Синий"});
+            this.comboBox1.Location = new System.Drawing.Point(164, 10);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(873, 482);
+            this.Controls.Add(this.GroupBox1);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.GroupBox1.ResumeLayout(false);
+            this.GroupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox GroupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
 
--- GSP_LR_2_3_IAIT_9/Form1.cs
+++ GSP_LR_2_3_IAIT_9/Form1.cs
@@ -1,25 +1,203 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GSP_LR_2_3_IAIT_9
 {
     public partial class Form1 : Form
     {
+        Graphics g;
+        Pen DrawPen = new Pen(Color.Black, 1);
+        int FillType = 0; // По умолчанию заливка построчная 
+        List<Point> VertexList = new List<Point>(); // Список вершин многоугольника
         public Form1()
         {
             InitializeComponent();
+            g = pictureBox1.CreateGraphics(); //инициализация графики
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
-
+            // Полная очистка экрана и списка вершин, после нажатия на кнопку
+            g.Clear(Color.White);
+            VertexList.Clear();
         }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    DrawPen.Color = Color.Black;
+                    break;
+                case 1:
+                    DrawPen.Color = Color.Red;
+                    break;
+                case 2:
+                    DrawPen.Color = Color.Green;
+                    break;
+                case 3:
+                    DrawPen.Color = Color.Blue;
+                    break;
+            }
+        }
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FillType = comboBox2.SelectedIndex;
+        }
+
+        private void FillByOrient()
+        {
+            Point pmin = VertexList[0];
+            Point pmax = VertexList[0];
+            int ymin = 0;
+            int ymax = pictureBox1.Height;
+            foreach (Point p in VertexList)
+            {
+                if (p.Y > pmax.Y) pmax = p;
+                if (p.Y < pmin.Y) pmin = p;
+            }
+            ymin = pmin.Y < ymin ? ymin : pmin.Y;
+            ymax = pmax.Y > ymax ? ymax : pmax.Y;
+            // получаем индекс наибольшего элемента
+            int j = VertexList.IndexOf(pmax);
+            int jl = j == 0 ? VertexList.Count - 1 : j - 1;
+            int jr = j == VertexList.Count - 1 ? 0 : j + 1;
+            double s = ((VertexList[jl].X * VertexList[j].Y)
+                + (VertexList[jl].Y * VertexList[jr].X)
+                + (VertexList[j].X * VertexList[jr].Y)
+                - (VertexList[j].Y * VertexList[jr].X)
+                - (VertexList[jl].Y * VertexList[j].X)
+                - (VertexList[jl].X * VertexList[jr].Y)
+            ) / 2;
+            bool CW = s < 0;
+            if (CW)
+            {
+                for (int y = 0; y < ymin; y++)
+                {
+                    g.DrawLine(DrawPen, new Point(0, y), new Point(pictureBox1.Width, y));
+                }
+            }
+            List<int> Xr = new List<int>();
+            List<int> Xl = new List<int>();
+            for (int y = ymin; y <= ymax; y++)
+            {
+                Xr.Clear();
+                Xl.Clear();
+                for (int i = 0; i < VertexList.Count; i++)
+                {
+                    int k = i < VertexList.Count - 1 ? i + 1 : 0;
+                    if (((VertexList[i].Y < y) && (VertexList[k].Y >= y)) || ((VertexList[i].Y >= y) && (VertexList[k].Y < y)))
+                    {
+                        // формула определения x координаты пересечения двух отрезков, заданных вершинами
+                        double x = VertexList[i].X + ((VertexList[k].X - VertexList[i].X) * (y - VertexList[i].Y)) / (VertexList[k].Y - VertexList[i].Y);
+                        if (VertexList[k].Y - VertexList[i].Y > 0)
+                        {
+                            Xr.Add((int)x);
+                        } else
+                        {
+                            Xl.Add((int)x);
+                        }
+                    }
+                }
+                if (CW)
+                {
+                    Xl.Add(0);
+                    Xr.Add(pictureBox1.Width);
+                }
+                Xl.Sort((a, b) => a.CompareTo(b)); // сортировка по возростанию
+                Xr.Sort((a, b) => a.CompareTo(b)); // сортировка по возростанию
+                for (int id = 0; id < Xl.Count && id < Xr.Count; id++)
+                {
+                    if (Xl[id] < Xr[id])
+                    {
+                        g.DrawLine(DrawPen, new Point(Xl[id], y), new Point(Xr[id], y));
+                    } 
+                }
+            }
+            if (CW)
+            {
+                for (int y = ymax; y < pictureBox1.Height; y++)
+                {
+                    g.DrawLine(DrawPen, new Point(0, y), new Point(pictureBox1.Width, y));
+                }
+            }
+        }
+
+        private void FillByLine()
+        {
+            Point pmin = VertexList[0];
+            Point pmax = VertexList[0];
+            int ymin = 0;
+            int ymax = pictureBox1.Height;
+            foreach (Point p in VertexList)
+            {
+                if (p.Y > pmax.Y) pmax = p;
+                if (p.Y < pmin.Y) pmin = p;
+            }
+            ymin = pmin.Y < ymin ? ymin : pmin.Y;
+            ymax = pmax.Y > ymax ? ymax : pmax.Y;
+
+            List<int> xBoundaries = new List<int>();
+
+            for (int y = ymin; y <= ymax; y++)
+            {
+                xBoundaries.Clear();
+                for (int i = 0; i < VertexList.Count ; i++)
+                {
+                    int k = i < VertexList.Count - 1 ? i + 1 : 0;
+                    if (((VertexList[i].Y < y) && (VertexList[k].Y >= y)) || ((VertexList[i].Y >= y) && (VertexList[k].Y < y)))
+                    {
+                        // формула определения x координаты пересечения двух отрезков, заданных вершинами
+                        double x = VertexList[i].X + ((VertexList[k].X - VertexList[i].X) * (y - VertexList[i].Y)) / (VertexList[k].Y - VertexList[i].Y);
+                        xBoundaries.Add((int)x);
+                    }
+                }
+                xBoundaries.Sort((a, b) => a.CompareTo(b)); // сортировка по возростанию
+                for (int el = 0; el < xBoundaries.Count - 1; el += 2)
+                {
+                    g.DrawLine(DrawPen, new Point(xBoundaries[el], y), new Point(xBoundaries[el + 1], y));
+                }
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            g = pictureBox1.CreateGraphics(); //реинициализация графики
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                VertexList.Add(new Point() { X = e.X, Y = e.Y });
+                g.DrawEllipse(DrawPen, e.X - 2, e.Y - 2, 5, 5);
+                int pointCount = VertexList.Count - 1;
+                if (pointCount > 0) {
+                    g.DrawLine(DrawPen, VertexList[pointCount - 1], VertexList[pointCount]);
+                }
+            } else if (e.Button == MouseButtons.Right)
+            {
+                int pointCount = VertexList.Count - 1;
+                if (pointCount < 2)
+                {
+                    MessageBox.Show("Введено слишком мало точек для существования многоугольника, отрисовка невозможна");
+                    return;
+                }
+                g.DrawLine(DrawPen, VertexList[pointCount - 1], VertexList[pointCount]);
+                g.DrawLine(DrawPen, VertexList[pointCount], VertexList[0]); // соединяем первую и последнюю точки
+                if (FillType == 0)
+                {
+                    FillByLine();
+                } else
+                {
+                    FillByOrient();
+                }
+                VertexList.Clear();
+            }
+        }
+
     }
 }
